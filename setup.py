d67cfea166a66f6e1233ff771c916db24deaf890
Kevin Corbin <kecorbin@cisco.com>
2015-04-24 12:30:59 -0500

    pypi1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Kevin Corbin',
   author_email = 'kecorbin@cisco.com',
   url = 'https://github.com/kecorbin/webarya', # use the URL to the github repo
-  download_url = 'https://github.com/kecorbin/webarya', # I'll explain this in a second
+  download_url = 'https://github.com/kecorbin/webarya/tarball/0.1', # I'll explain this in a second
   keywords = ['arya', 'apic', 'aci', 'cisco'],
   classifiers = [],
 )
