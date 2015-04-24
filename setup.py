2d79f8de176e217b01ee56437ba217dfd0582ea6
Kevin Corbin <kecorbin@cisco.com>
2015-04-24 12:28:18 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'webarya',
+  packages = ['webarya'],
+  version = '0.1',
+  description = 'Flask front-end for Arya',
+  author = 'Kevin Corbin',
+  author_email = 'kecorbin@cisco.com',
+  url = 'https://github.com/kecorbin/webarya', # use the URL to the github repo
+  download_url = 'https://github.com/kecorbin/webarya', # I'll explain this in a second
+  keywords = ['arya', 'apic', 'aci', 'cisco'],
+  classifiers = [],
+)
